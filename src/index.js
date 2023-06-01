3d107989e61b1bf6e33c3d2b9d48d5025cb20db4
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-01 23:22:56 +0300

    breeds

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
+import { fetchBreeds } from "./cat-api";
 import SlimSelect from 'slim-select';
+fetchBreeds()
 
 new SlimSelect({
   select: '#single',
 });
 
-const YOUR_API_KEY = `live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86`;
-const url = `https://api.thecatapi.com/v1/images/search`;
-fetch('https://api.thecatapi.com/v1/images/search?limit=10')
-  .then(respons => respons.json())
-  .then(json => console.log(json));
+
