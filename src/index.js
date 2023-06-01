f4516c08edd3520c1cb772586a275459fb3f0048
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-01 20:47:33 +0300

    trash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import SlimSelect from 'slim-select';
+
+new SlimSelect({
+  select: '#single',
+});
+
 const YOUR_API_KEY = `live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86`;
 const url = `https://api.thecatapi.com/v1/images/search`;
-fetch(
-  `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${YOUR_API_KEY}`
-);
+fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+  .then(respons => respons.json())
+  .then(json => console.log(json));
