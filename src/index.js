a570e354aeefe9676e20c894cb807965326caf41
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-01 20:04:57 +0300

    chek

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const YOUR_API_KEY = `live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86`;
+const url = `https://api.thecatapi.com/v1/images/search`;
+fetch(
+  `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${YOUR_API_KEY}`
+);
