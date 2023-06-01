1f81249d3b7001b944b7fd2a300f0f0f2083d109
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-01 17:05:35 +0000

    Deploying to gh-pages from @ Olguca2020/goit-js-hw-10@a570e354aeefe9676e20c894cb807965326caf41 🚀

---
+++ index.357a73bb.js
@@ -0,0 +1,2 @@
+fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=".concat("live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86"));
+//# sourceMappingURL=index.357a73bb.js.map
