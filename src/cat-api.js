2e023c9cd1ab76ed465d1c655a8eb8a6629782dd
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-02 22:32:29 +0300

    3

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,16 @@
 function fetchBreeds() {
-const YOUR_API_KEY = `live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86`;
-const url = `https://api.thecatapi.com/v1/images/search`;
-    return fetch(`https://api.thecatapi.com/v1/breeds`)
-      .then(respons => respons.json())
-        .then(resp => { return resp.map(breed => breed.name) });  
+  const YOUR_API_KEY = `live_sZJSF2r3b7RaQcpCRE4JWCZQFkPyecbqN78KnhGUiHv7pDzuobrRQWETam8vMi86`;
+  const url = `https://api.thecatapi.com/v1/`;
+  return fetch(`${url}breeds`, {
+    headers: {
+      'x-api-key': YOUR_API_KEY,
+    },
+  }).then(respons => {
+    if (!respons.ok) {
+      throw new Error();
+    }
+    return respons.json();
+  });    
 };
+
 export { fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import { fetchBreeds } from "./cat-api";
-import SlimSelect from 'slim-select';
-console.log(fetchBreeds());
-
 new SlimSelect({
   select: '#single',
 });
+
+fetchBreeds()
+  .then(res => {    
+    const breedSelect = document.querySelector(`#single`);    
+    res.forEach(breed => {
+      const optionElement = document.createElement(`option`);
+      optionElement.value = breed.id;
+      optionElement.textContent = breed.name;      
+      breedSelect.appendChild(optionElement);
+    });
+  })
+  .catch (error => {
+    console.error('Сталася помилка:', error);
+  });
+
+
 
 
