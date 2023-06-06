96d6a3a66e9c25b8c8afb73a4db06e303e08001e
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-06-06 22:14:03 +0300

    9

--- src/index.js
+++ src/index.js
@@ -1,58 +1,66 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
-
-
 const refs = {
   selectEl: document.querySelector('#selectElement'),
   loaderEl: document.querySelector(`.loader`),
   errorEl: document.querySelector(`.error`),
   catInfoContainer: document.querySelector(`.cat-info`),
 };
-
+const select = new SlimSelect({
+  select: '#selectElement',
+});
 function showLoader() {
   refs.loaderEl.classList.remove('is-hidden');
 }
-
 function hideLoader() {
   refs.loaderEl.classList.add('is-hidden');
 }
-
 function showContainer() {
   refs.catInfoContainer.classList.remove('is-hidden');
 }
-
 function hideContainer() {
   refs.catInfoContainer.classList.add('is-hidden');
 }
 function showBreedSelect() {
   refs.selectEl.classList.remove('is-hidden');
 }
-
 function hideBreedSelect() {
   refs.selectEl.classList.add('is-hidden');
 }
-
 function showError() {
   refs.errorEl.classList.remove('is-hidden');
 }
-
 function hideError() {
   refs.errorEl.classList.add('is-hidden');
 }
-
+fetchBreeds()
+  .then(res => {
+    const options = res.map(({ reference_image_id, name }) => ({
+      value: reference_image_id,
+      text: name,
+    }));
+    select.setData(options);
+    select.selectEl.classList.remove(`is-hidden`);
+    refs.loaderEl.classList.add(`is-hidden`);
+  })
+  .catch(err => {
+    refs.loaderEl.classList.add(`is-hidden`);
+    showError();
+    console.log(err);
+  });
 let firstLoade = true;
 refs.selectEl.addEventListener('change', event => {
-  const breedId = event.target.value;
-  showBreedSelect();
-  showLoader();
   hideError();
+  if (firstLoade) {
+    firstLoade = false;
+    return;
+  }
+  const breedId = event.target.value;
+  
+  showLoader();
+  hideContainer();
   fetchCatByBreed(breedId)
     .then(res => {
-      if (firstLoade) {
-        firstLoade = false;
-        return;
-      }
-      
       const {
         url,
         breeds: [{ name, description, temperament }],
@@ -65,42 +73,13 @@ refs.selectEl.addEventListener('change', event => {
         <p><strong>Temperament:</strong> ${temperament}</p>
         </div>
       `;
-
       refs.catInfoContainer.innerHTML = marcup;
       hideLoader();
+      showContainer();
       refs.selectEl.value = '';
     })
     .catch(err => {
       hideLoader();
-      hideBreedSelect();
-      hideContainer();
       showError();
-      Notiflix.Notify.failure(`Error: ${err.message}`);
     });
-  
-});
-
-fetchBreeds()
-  .then(res => {
-    const options = res.map(({ reference_image_id, name }) => ({
-      value: reference_image_id,
-      text: name,
-    }));
-    options.unshift({ value: '', text: '' });
-    select.setData(options);
-    select.selectEl.classList.remove(`is-hidden`);
-    refs.loaderEl.classList.add(`is-hidden`);
-    
-  })
-  .catch(err => {
-    hideLoader();
-    hideBreedSelect();
-    hideContainer();
-    showError();
-    Notiflix.Notify.failure(`Error: ${err.message}`);
-    showBreedSelect();
-  });
-
-const select = new SlimSelect({
-  select: '#selectElement',
 });
